5a795a5d9887122c92da6d2783d714e5e10a0c60
dthoang1 <jack08022018>
2022-09-30 10:24:06 +0700

    a

--- src/config/configuration.yaml.ts
+++ src/config/configuration.yaml.ts
@@ -1,9 +1,10 @@
 import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
-const YAML_CONFIG = 'dev.yaml';
 
+// https://progressivecoder.com/one-stop-guide-to-nestjs-config-environment-variables/
 export default () => {
+    const YAML_CONFIG = `properties.${process.env.NODE_ENV}.yaml`;
     return yaml.load(
         readFileSync(join(__dirname, YAML_CONFIG), 'utf8'),
     )as Record<string, any>
